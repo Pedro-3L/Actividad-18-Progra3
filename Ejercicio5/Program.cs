6bceae9c74219e40ad5ddcb04ed232bcf60c5ffc
lando133p@gmail.com <lando133p@gmail.com>
2026-04-10 08:46:11 -0600

    adicion ejerciciop 5

---
+++ Ejercicio5/Program.cs
@@ -0,0 +1,27 @@
+﻿Usuario u = new Usuario();
+u.Contrasena = "12345678";
+Console.WriteLine(u.Contrasena);
+
+class Usuario
+{
+    private string contrasena;
+    
+    public string Contrasena
+    {
+        get
+        {
+            if (contrasena.Length >= 8)
+            {
+                return "****";
+            }
+            else
+            {
+                return "Contraseña insegura";
+            }
+        }
+        set
+        {
+            contrasena = value;
+        }
+    }
+}
