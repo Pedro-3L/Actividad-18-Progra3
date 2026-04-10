840c4f3b0291512d284a24c36b36859046ff140b
ALUMNO <jm.gcaxaj@udeo.edu.gt>
2026-04-10 08:14:38 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿class CuentaBancaria
+{
+	private double saldo;
+
+	public	double Saldo
+	{
+		get 
+		{ 
+			return saldo; 
+		}
+		set
+		{
+			if (value >= 0)
+            {
+				 saldo = value;
+
+			}
+			else
+			{
+                Console.WriteLine("Saldo no disponible");
+			}
+     
+		}
+	}
+
+}
