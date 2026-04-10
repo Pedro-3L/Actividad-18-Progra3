91cb797e24e87c28f2947999a36deed1b841d6f3
lando133p@gmail.com <lando133p@gmail.com>
2026-04-10 08:33:03 -0600

    Adicion Ejercicio 2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,30 @@
+﻿Estudiante s = new Estudiante();
+s.Edad = 200;
+Console.WriteLine(s.Edad);
+
+class Estudiante
+{
+    private double edad;
+
+    public double Edad
+    {
+        get
+        {
+            return edad;
+        }
+        set
+        {
+            if (value >= 0 && value<=100)
+            {
+                edad = value;
+
+            }
+            else
+            {
+                edad = 0;
+            }
+
+        }
+    }
+
+}
