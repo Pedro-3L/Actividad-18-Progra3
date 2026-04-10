c171051a90464ce6c47798f991f367358f5ac1cb
lando133p@gmail.com <lando133p@gmail.com>
2026-04-10 08:41:31 -0600

    adicion ejercicio 4

---
+++ Ejercicio4/Program.cs
@@ -0,0 +1,29 @@
+﻿Empleado e = new Empleado();
+e.Salario = 2000;
+Console.WriteLine(e.Salario);
+
+class Empleado
+{
+    private double salario;
+
+    public double Salario
+    {
+        get
+        {
+            return salario;
+        }
+        set
+        {
+            if (value >= 5000)
+            {
+                salario = value;
+
+            }
+            else
+            {
+                Console.WriteLine("Salario invalido");
+            }
+
+        }
+    }
+}
